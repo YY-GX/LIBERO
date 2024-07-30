9fb38a3cfb30d211669bf9eac89a2e5928b59584
yy-gx <yyang9923@gmail.com>
2024-07-30 11:18:41 -0400

    modify evaluate.py

--- libero/libero/__init__.py
+++ libero/libero/__init__.py
@@ -67,9 +67,11 @@ if not os.path.exists(config_file):
     # Create a default config file
 
     default_path_dict = get_default_path_dict()
-    answer = input(
-        "Do you want to specify a custom path for the dataset folder? (Y/N): "
-    ).lower()
+    # answer = input(
+    #     "Do you want to specify a custom path for the dataset folder? (Y/N): "
+    # ).lower()
+    # yy: all no for this answer
+    answer = "n"
     if answer == "y":
         # If the user wants to specify a custom storage path, prompt them to enter it
         custom_dataset_path = input(
