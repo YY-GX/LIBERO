bb489e338fcbfeeb0ffcb330102ed5c036cb9065
yy-gx <yyang9923@gmail.com>
2024-09-01 18:21:48 -0400

    debug why training skills so slow

--- libero/libero/benchmark/yy_suite_task_map.py
+++ libero/libero/benchmark/yy_suite_task_map.py
@@ -2,7 +2,7 @@ yy_task_map = {
     "yy_try": [
         # "KITCHEN_SCENE10_put_the_black_bowl_in_the_top_drawer_of_the_cabinet"
         # "KITCHEN_SCENE_put_the_black_bowl_in_the_top_drawer_of_the_cabinet",
-        "SHELF_TABLE_SCENE_moving_potato_from_shelf_to_the_plate_on_the_table",
+        "SHELF_TABLE_SCENE_moving_popcorn_from_table_to_topside_of_wooden_shelf_demo",
     ],
 
 }
