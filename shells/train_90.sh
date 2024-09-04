c3784de24264749d74b1734bf2ce25ebb449cbf2
yy-gx <yyang9923@gmail.com>
2024-09-04 16:40:58 -0400

    eval modified task on pre-trained

--- shells/train_90.sh
+++ shells/train_90.sh
@@ -15,6 +15,6 @@ sbatch --gpus 1 --cpus-per-task=8 \
        python lifelong/train_skills.py version=\"shelf_move_potato_v1_finetune\" data.seq_len=30 train.batch_size=32 train.optimizer.kwargs.lr=1e-5 && \
        python lifelong/train_skills.py version=\"shelf_move_potato_v1_finetune\" data.seq_len=30 train.batch_size=64 train.optimizer.kwargs.lr=1e-4
        "
-#export CUDA_VISIBLE_DEVICES=7 && export MUJOCO_EGL_DEVICE_ID=7 && python lifelong/train_skills.py is_split=false is_customize_task=true version="train_base_90_v0" benchmark_name=libero_90 policy=bc_transformer_policy lifelong=base
+export CUDA_VISIBLE_DEVICES=7 && export MUJOCO_EGL_DEVICE_ID=7 && python lifelong/train_skills.py is_split=false is_customize_task=true version="train_base_90_v0" benchmark_name=libero_90 policy=bc_transformer_policy lifelong=base
 
 
