e6a6c28325778fc986deb9f69fc32913561753fc
yy-gx <yyang9923@gmail.com>
2024-09-11 19:32:48 -0400

    add train_skills_new.py

--- libero/libero/benchmark/__init__.py
+++ libero/libero/benchmark/__init__.py
@@ -277,8 +277,5 @@ class yy_try(Benchmark):
 class modified_libero(Benchmark):
     def __init__(self, task_order_index=0, n_tasks_=1):
         super().__init__(task_order_index=task_order_index, n_tasks_=n_tasks_)
-        assert (
-            task_order_index == 0
-        ), "[error] currently only support task order for 10-task suites"
         self.name = "modified_libero"
         self._make_benchmark()
