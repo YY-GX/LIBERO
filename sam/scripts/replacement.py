3c1796d4833e107309275be3284d9c6fa5e75176
yy-gx <yyang9923@gmail.com>
2024-09-22 21:12:20 -0400

    add train_skills_new.py

--- sam/scripts/replacement.py
+++ sam/scripts/replacement.py
@@ -11,6 +11,7 @@ from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 import groundingdino
 
+
 def load_image(image):
     transform = groundingdino.T.Compose(
         [
