62514a039c591c3b56c519c7b04cdb4b0c41d524
yy-gx <yyang9923@gmail.com>
2024-09-24 18:45:47 -0400

    replacement.py - version that only contains replacement diff (i.e., directly change different area)

--- sam/scripts/replacement.py
+++ sam/scripts/replacement.py
@@ -315,7 +315,7 @@ def OSM_correction(
         modified_img,
         text_prompts,
         output_dir,
-        color_threshold=10,
+        color_threshold=30,
         area_fraction=0.05,
         is_debug=True
 ):
