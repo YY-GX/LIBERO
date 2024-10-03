abdbc31f5bfd411406dee561ebd5546cc2b518d0
yy-gx <yyang9923@gmail.com>
2024-10-02 22:20:07 -0400

    finish eval_skill_chain_new.py

--- shells/long_horizon_task.sh
+++ shells/long_horizon_task.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+cd /mnt/arc/yygx/pkgs_baselines/LIBERO/ &&
+
 export CUDA_VISIBLE_DEVICES=0 && export MUJOCO_EGL_DEVICE_ID=0 && python libero/lifelong/eval_skill_chain_new.py --task_order_index 8 --seed 10000 --device_id 0 --benchmark "libero_90" --model_path_folder "/mnt/arc/yygx/pkgs_baselines/LIBERO/libero/experiments/libero_90/training_eval_skills_original_env/Sequential/BCRNNPolicy_seed10000/all" > out_long_2.out 2>&1 &
 
 export CUDA_VISIBLE_DEVICES=1 && export MUJOCO_EGL_DEVICE_ID=1 && python libero/lifelong/eval_skill_chain_new.py --task_order_index 9 --seed 10000 --device_id 0 --benchmark "libero_90" --model_path_folder "/mnt/arc/yygx/pkgs_baselines/LIBERO/libero/experiments/libero_90/training_eval_skills_original_env/Sequential/BCRNNPolicy_seed10000/all" > out_long_3.out 2>&1 &
