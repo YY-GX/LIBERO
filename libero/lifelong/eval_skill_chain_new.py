32b1efb1837552c8037df7c7979c75dc44f3f2d5
yy-gx <yyang9923@gmail.com>
2024-10-24 17:23:45 -0400

    optimize script for real-long-horizon-exp

--- libero/lifelong/eval_skill_chain_new.py
+++ libero/lifelong/eval_skill_chain_new.py
@@ -230,9 +230,6 @@ def main():
         # yy: formal start of the evaluation
         with torch.no_grad():
             while steps < (cfg.eval.max_steps * n_tasks):
-
-                t_0 = time.time()
-
                 steps += 1
                 if steps % (cfg.eval.max_steps // 30) == 0:
                     print(f"[INFO] Steps: {steps}; Task Indexes: {task_indexes}.", flush=True)
@@ -362,10 +359,10 @@ def main():
                     task_emb = benchmark.get_task_emb(task_idx_ls[task_indexes[k]])
                     task_embs.append(task_emb)
                 task_embs = torch.stack(task_embs)
-                t_1 = time.time()
+
                 # Obtain torch data - main inputs: obs + task_embs
                 data = raw_obs_to_tensor_obs(obs, task_embs, cfg, is_sequential_env=True)
-                t_2 = time.time()
+
                 for k in range(env_num):
                     data_cp = copy.deepcopy(data)
                     algo = algo_ls[task_indexes[k]]
@@ -382,8 +379,6 @@ def main():
                 # yy: reset robot arm if move to a new skill. Modify the obs as well.
                 obs = reset_env_init_states(env, obs, info, init_states_ls, env_num, task_indexes)
 
-                t_3 = time.time()
-
                 video_writer_agentview.append_vector_obs(
                     obs, dones, camera_name="agentview_image"
                 )
@@ -397,11 +392,7 @@ def main():
                 if all(dones):
                     break
 
-                print(f"t_1 - t_0: {t_1 - t_0}")
-                print(f"t_2 - t_1: {t_2 - t_1}")
-                print(f"t_3 - t_2: {t_3 - t_2}")
 
-                exit(0)
 
             for k in range(env_num):
                 num_success += int(dones[k])
