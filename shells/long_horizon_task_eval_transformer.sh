e1f6594d57e044cd3a971903597d54f6d9a24083
yy-gx <yyang9923@gmail.com>
2024-10-25 13:47:03 -0400

    optimize script for real-long-horizon-exp

--- shells/long_horizon_task_eval_transformer.sh
+++ shells/long_horizon_task_eval_transformer.sh
@@ -4,6 +4,6 @@ cd /mnt/arc/yygx/pkgs_baselines/LIBERO/ || exit
 
 for task_order_index in {7..16}; do
     echo "Starting task_order_index: $task_order_index"
-    python libero/lifelong/eval_skill_chain_new.py --task_order_index $task_order_index --seed 10000 --device_id 0 --benchmark "libero_90" --model_path_folder "/mnt/arc/yygx/pkgs_baselines/LIBERO/libero/experiments/libero_90/skill_policies_without_wrist_camera_view/Sequential/BCTransformerPolicy_seed10000/all"
+    export CUDA_VISIBLE_DEVICES=0 && export MUJOCO_EGL_DEVICE_ID=0 && python libero/lifelong/eval_skill_chain_new.py --task_order_index $task_order_index --seed 10000 --device_id 0 --benchmark "libero_90" --model_path_folder "/mnt/arc/yygx/pkgs_baselines/LIBERO/libero/experiments/libero_90/skill_policies_without_wrist_camera_view/Sequential/BCTransformerPolicy_seed10000/all"
     echo "Completed task_order_index: $task_order_index"
 done
