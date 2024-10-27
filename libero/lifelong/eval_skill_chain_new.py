86a28e4dc3704e9fd292d6003304e9bd505fcd5e
yy-gx <yyang9923@gmail.com>
2024-10-27 13:33:50 -0400

    optimize script for real-long-horizon-exp

--- libero/lifelong/eval_skill_chain_new.py
+++ libero/lifelong/eval_skill_chain_new.py
@@ -142,6 +142,7 @@ def main():
         algo = safe_device(get_algo_class(algo_map["base"])(n_tasks, cfg), cfg.device)
         algo.policy.load_state_dict(sd)
         algo.eval()
+        print(f">> task_id: {task_id}, policy class: {algo.policy}")
         # yy: algo_ls here
         algo_ls.append(algo)
 
@@ -225,11 +226,14 @@ def main():
         # yy: formal start of the evaluation
         with torch.no_grad():
             while steps < (cfg.eval.max_steps * n_tasks):
+                print("--------------------------------------------------------------------")
+                print(steps)
                 steps += 1
                 if steps % (cfg.eval.max_steps // 30) == 0:
                     print(f"[INFO] Steps: {steps}; Task Indexes: {task_indexes}.", flush=True)
                     print(f"Evaluation takes {t.get_middle_past_time()} seconds", flush=True)
-
+                    if steps == 90:
+                        exit(0)
 
                 actions = np.zeros((1, 7))
                 # For the 20 envs, each may have different language descriptions, i.e., task_emb
@@ -243,6 +247,7 @@ def main():
                 data = raw_obs_to_tensor_obs(obs, task_embs, cfg, is_sequential_env=True)
 
                 for k in range(env_num):
+                    print(f"env_idx: {k}; task_idx: {task_indexes[k]}")
                     data_cp = copy.deepcopy(data)
                     algo = algo_ls[task_indexes[k]]
                     # only take the k'th value for data
